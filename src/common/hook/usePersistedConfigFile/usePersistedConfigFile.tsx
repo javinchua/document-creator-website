9c403e3414a89649d8f8db25dd7de49f49315c12
Poh Say Keong <pohsaykeong@gmail.com>
2022-03-27 19:43:33 +0800

    return empty object in persistent hooks
    This reverts commit 79ed00afdd9cec386b3d0afa58a95f8a9029b3f7.

--- src/common/hook/usePersistedConfigFile/usePersistedConfigFile.tsx
+++ src/common/hook/usePersistedConfigFile/usePersistedConfigFile.tsx
@@ -1,16 +1,18 @@
 import createPersistedState from "use-persisted-state";
 import { ConfigFile } from "../../../types";
 
-const useConfigFile = createPersistedState<ConfigFile>("CONFIG_FILE");
+const useConfigFile = createPersistedState<ConfigFile | Record<string, null>>("CONFIG_FILE");
 
 export const usePersistedConfigFile = (): {
   configFile?: ConfigFile;
   setConfigFile: (configFile?: ConfigFile) => void;
 } => {
-  const [configFileFromStorage, setConfigFileInStorage] = useConfigFile();
-  const configFile = Object.keys(configFileFromStorage as ConfigFile).length === 0 ? undefined : configFileFromStorage;
+  // Using empty object to initialize config file due to bug with deserializing "undefined"
+  const [configFileFromStorage, setConfigFileInStorage] = useConfigFile({});
+  const configFile =
+    Object.keys(configFileFromStorage).length === 0 ? undefined : (configFileFromStorage as ConfigFile);
   const setConfigFile = (config?: ConfigFile): void => {
-    setConfigFileInStorage(config);
+    setConfigFileInStorage(config ? config : {});
   };
   return { configFile, setConfigFile };
 };
--- src/common/hook/usePersistedWalletFile/usePersistedWalletFile.tsx
+++ src/common/hook/usePersistedWalletFile/usePersistedWalletFile.tsx
@@ -1,16 +1,17 @@
 import createPersistedState from "use-persisted-state";
 import { Wallet } from "ethers";
 
-const useWalletFile = createPersistedState<Wallet>("WALLET_FILE");
+const useWalletFile = createPersistedState<Wallet | Record<string, null>>("WALLET_FILE");
 
 export const usePersistedWalletFile = (): {
   walletFile?: Wallet;
   setWalletFile: (walletFile?: Wallet) => void;
 } => {
-  const [walletFileFromStorage, setWalletFileInStorage] = useWalletFile();
-  const walletFile = Object.keys(walletFileFromStorage as Wallet).length === 0 ? undefined : walletFileFromStorage;
+  // Using empty object to initialize config file due to bug with deserializing "undefined"
+  const [walletFileFromStorage, setWalletFileInStorage] = useWalletFile({});
+  const walletFile = Object.keys(walletFileFromStorage).length === 0 ? undefined : (walletFileFromStorage as Wallet);
   const setWalletFile = (wallet?: Wallet): void => {
-    setWalletFileInStorage(wallet);
+    setWalletFileInStorage(wallet ? wallet : {});
   };
   return { walletFile, setWalletFile };
 };
